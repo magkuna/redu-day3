6d8eec157066316f83df5af0bda9d199e5857c34
magkuna <magkuna@gmail.com>
2019-04-17 11:12:18 +0200

    hunk installes and aplly/middleWare and compose  to store.js

--- src/Users.js
+++ src/Users.js
@@ -2,20 +2,32 @@ import React from 'react'
 
 import { setUsersActionCreator } from './state/users'
 
+
 import { connect } from 'react-redux'
 
 const Users = (props) => {
-    if(props._users === null){
-    fetch('https://randomuser.me/api')
-        .then(response => response.json())
-        .then(data => {
-            props._setUsers(data.results)
-        })
-}
+    if (props._users === null) {
+        fetch('https://randomuser.me/api')
+            .then(response => response.json())
+            .then(data => {
+                props._setUsers(data.results)
+            })
+    }
     return (
         <div>
-            USERS
-    </div>
+            {
+                props._users &&
+                props._users.map(
+                    user => (
+                        <div
+                        key= {user.login.uuid}
+                        >
+                            {user.name.first}
+                        </div>
+                    )
+                )
+            }
+        </div>
     )
 }
 
--- src/store.js
+++ src/store.js
@@ -1,12 +1,18 @@
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 
 import users from './state/users'
+
+import thunk from 'redux'
 
 const rootReducer = combineReducers({
     users
 })
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 export const store = createStore(
     rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    composeEnhancers(
+        applyMiddleware(thunk)
+    )
 )
